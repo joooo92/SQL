163e75e05e2cba6b202ce2eec40a58c376d15f83
jooooo92 <guswn8013@gmail.com>
2023-05-19 18:30:44 +0900

    23.05.19 추가

--- 12서브쿼리QUIZ.sql
+++ 12서브쿼리QUIZ.sql
@@ -27,12 +27,16 @@ SELECT *
 FROM EMPLOYEES E LEFT JOIN DEPARTMENTS D ON E.MANAGER_ID = D.MANAGER_ID
 WHERE E.MANAGER_ID = 100;
 
+--쌤 답변
+SELECT * 
+FROM EMPLOYEES
+WHERE DEPARTMENT_ID = (SELECT DEPARTMENT_ID FROM DEPARTMENTS WHERE MANAGER_ID = 100);
 
 
 
 --문제 3.
 ---EMPLOYEES테이블에서 “Pat”의 manager_id보다 높은 manager_id를 갖는 모든 사원의 데이터를 출력하세요
----EMPLOYEES테이블에서 “James”(2명)들의 manager_id와 갖는 모든 사원의 데이터를 출력하세요.
+---EMPLOYEES테이블에서 “James”(2명)들의 manager_id와 같는 모든 사원의 데이터를 출력하세요.
 SELECT *
 FROM EMPLOYEES
 WHERE MANAGER_ID > (SELECT MANAGER_ID
@@ -55,9 +59,19 @@ FROM (SELECT FIRST_NAME,
         FROM(SELECT * 
              FROM EMPLOYEES 
              ORDER BY FIRST_NAME DESC)
-)
+      )
 WHERE RN >= 41 AND RN <= 50;
-        
+
+--쌤 답변 --?? 행 번호, 이름만 출력하는거 아니었나
+SELECT *
+FROM (SELECT E.*, 
+             ROWNUM AS RN
+        FROM(SELECT * 
+             FROM EMPLOYEES 
+             ORDER BY FIRST_NAME DESC) E
+      )
+WHERE RN >= 41 AND RN <= 50;
+
 
 
 
@@ -68,12 +82,25 @@ WHERE RN >= 41 AND RN <= 50;
 SELECT *
 FROM( SELECT HIRE_DATE,
             EMPLOYEE_ID,
-            FIRST_NAME,
+            FIRST_NAME ,
             PHONE_NUMBER,
              ROWNUM AS RN
       FROM(SELECT * FROM EMPLOYEES ORDER BY HIRE_DATE ASC)
 )
 WHERE RN >=31 AND RN <=40;
+
+--쌤답변
+SELECT *
+FROM(SELECT E.*,
+            ROWNUM RN
+        FROM( SELECT HIRE_DATE,
+                    EMPLOYEE_ID,
+                    FIRST_NAME || ' ' || LAST_NAME AS NAME,
+                    PHONE_NUMBER
+              FROM EMPLOYEES 
+              ORDER BY HIRE_DATE ) E
+    )          
+WHERE RN BETWEEN 31 AND 40;
 
 
 
@@ -89,6 +116,9 @@ SELECT E.EMPLOYEE_ID,
        D.DEPARTMENT_NAME
 FROM EMPLOYEES E LEFT JOIN DEPARTMENTS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY EMPLOYEE_ID;
+
+
+
 
 --문제 7.
 --문제 6의 결과를 (스칼라 쿼리)로 동일하게 조회하세요
@@ -152,7 +182,7 @@ SELECT * FROM COUNTRIES;
 
 --문제 11.
 --문제 10의 결과를 (스칼라 쿼리)로 동일하게 조회하세요
-
+--?? 요긴 오더바이 안쓰나유
 SELECT L.LOCATION_ID,
        L.STREET_ADDRESS,
        L.CITY,
@@ -160,3 +190,122 @@ SELECT L.LOCATION_ID,
        (SELECT C.COUNTRY_NAME FROM COUNTRIES C WHERE L.COUNTRY_ID = C.COUNTRY_ID) AS COUNTRY_NAME
 FROM LOCATIONS L 
 ORDER BY COUNTRY_NAME;
+
+--------------------------------------------------------------------------
+--어려운 문제 등장!
+--문제 12. 
+--employees테이블, departments테이블을 left조인 hire_date를 오름차순 기준으로 1-10번째 데이터만 출력합니다
+--조건) rownum을 적용하여 번호, 직원아이디, 이름, 전화번호, 입사일, 부서아이디, 부서이름 을 출력합니다.
+--조건) hire_date를 기준으로 오름차순 정렬 되어야 합니다. rownum이 틀어지면 안됩니다.
+
+SELECT *
+FROM(SELECT ROWNUM RN,
+            A.*
+        FROM(SELECT E.EMPLOYEE_ID,
+                    E.FIRST_NAME,
+                    E.PHONE_NUMBER,
+                    E.HIRE_DATE,
+                    E.DEPARTMENT_ID,
+                    D.DEPARTMENT_NAME
+             FROM EMPLOYEES E 
+             LEFT JOIN DEPARTMENTS D
+             ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+             ORDER BY HIRE_DATE) A
+    )         
+WHERE RN > 0 AND RN <= 10;
+
+
+
+--문제 13. 
+----EMPLOYEES 과 DEPARTMENTS 테이블에서 JOB_ID가 SA_MAN 사원의 정보의 LAST_NAME, JOB_ID, 
+--DEPARTMENT_ID,DEPARTMENT_NAME을 출력하세요
+
+SELECT E.LAST_NAME, E.JOB_ID, E.DEPARTMENT_ID, D.DEPARTMENT_NAME
+FROM EMPLOYEES E LEFT JOIN DEPARTMENTS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+WHERE JOB_ID = 'SA_MAN';
+
+--쌤 답변
+SELECT E.LAST_NAME,
+       E.JOB_ID,
+       E.DEPARTMENT_ID,
+       D.DEPARTMENT_NAME
+FROM (SELECT * 
+        FROM EMPLOYEES
+        WHERE JOB_ID = 'SA_MAN')E
+JOIN DEPARTMENTS D
+ON E.DEPARTMENT_ID = D.DEPARTMENT_ID;
+
+
+
+
+--문제 14
+----DEPARTMENT테이블에서 각 부서의 ID, NAME, MANAGER_ID와 부서에 속한 인원수를 출력하세요.
+----인원수 기준 내림차순 정렬하세요.
+----사람이 없는 부서는 출력하지 뽑지 않습니다
+
+-- 쌤답변
+SELECT D.DEPARTMENT_ID, 
+       D.DEPARTMENT_NAME, 
+       D.MANAGER_ID,
+       E.인원수
+FROM DEPARTMENTS D
+INNER JOIN (SELECT DEPARTMENT_ID, 
+            COUNT(*) AS 인원수
+        FROM EMPLOYEES 
+        GROUP BY DEPARTMENT_ID) E 
+ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+ORDER BY 인원수 DESC;
+
+
+--문제 15
+----부서에 대한 정보 전부와, 주소, 우편번호, 부서별 평균 연봉을 구해서 출력하세요
+----부서별 평균이 없으면 0으로 출력하세요
+--쌤답변
+SELECT D.*,
+       NVL(E.SALARY,0) AS SALARY,
+       L.STREET_ADDRESS,
+       L.POSTAL_CODE
+FROM DEPARTMENTS D
+LEFT JOIN (SELECT DEPARTMENT_ID,
+                  TRUNC(AVG(SALARY)) AS SALARY
+           FROM EMPLOYEES
+           GROUP BY DEPARTMENT_ID)E
+ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+LEFT JOIN LOCATIONS L
+ON D.LOCATION_ID = L.LOCATION_ID;
+
+
+--문제 16
+---문제 15결과에 대해 DEPARTMENT_ID기준으로 내림차순 정렬해서 ROWNUM을 붙여 1-10데이터 까지만
+--출력하세요
+
+
+
+SELECT *
+FROM(SELECT ROWNUM RN,
+           X.* 
+    FROM(SELECT D.*,
+               NVL(E.SALARY,0) AS SALARY,
+               L.STREET_ADDRESS,
+               L.POSTAL_CODE
+        FROM DEPARTMENTS D
+        LEFT JOIN (SELECT DEPARTMENT_ID,
+                          TRUNC(AVG(SALARY)) AS SALARY
+                   FROM EMPLOYEES
+                   GROUP BY DEPARTMENT_ID)E
+        ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+        LEFT JOIN LOCATIONS L
+        ON D.LOCATION_ID = L.LOCATION_ID
+        ORDER BY D.DEPARTMENT_ID DESC
+        )X
+    )
+WHERE RN > 0 AND RN <= 20;
+
+
+
+
+
+
+
+
+
